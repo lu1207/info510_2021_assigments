97755b2a22f37aeedbcff0bb11e1a6660665e097
Zhiying Lu <luzy@CAS-C02DG4LDML85.local>
2021-06-24 16:56:20 -0500

    homework 1

--- assigment_8.R
+++ assigment_8.R
@@ -2,13 +2,12 @@
 
 Remove_non_numeric <- function(raw_data)
   {for (i in 1:length(names(raw_data))) 
-    {while (class(raw_data[,i]) != "numeric") 
-      {raw_data <- raw_data[,-c(i)]
+    {if (class(raw_data[,i]) != "numeric") 
+      {raw_data <- raw_data[,-i]
       }
-    }
+  }
   return(raw_data) 
 }
 
 
-
-
+Remove_non_numeric(iris)
