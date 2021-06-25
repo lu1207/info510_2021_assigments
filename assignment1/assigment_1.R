5451d28b3675b3443af90a20b2ae59f0737f9ed0
lu1207 <luzy05111036@163.com>
2021-06-24 19:11:15 -0500

    submit homework1_edit

--- assignment1/assigment_1.R
+++ assignment1/assigment_1.R
@@ -2,9 +2,9 @@
 
 Remove_non_numeric <- function(raw_data)
   {for (i in 1:length(names(raw_data))) 
-    {while (class(raw_data[,i]) != "numeric") 
+    {if (class(raw_data[,i]) != "numeric") 
       {raw_data <- raw_data[,-i]
-     break }
+       }
   }
   return(raw_data) 
 }
